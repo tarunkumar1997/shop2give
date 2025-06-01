08c58784a9068a5f0dca5eae152343fb1db83500
Elky Bachtiar <139325226+elky-bachtiar@users.noreply.github.com>
2025-06-01 10:41:55 +0200

    Fix string escaping in stripe configuration

--- src/stripe-config.ts
+++ src/stripe-config.ts
@@ -2,7 +2,7 @@ export const products = {
   'Cross Bracelet': {
     priceId: 'price_1RUZi7R19bsejBdlukMGNHJc',
     name: 'Cross Bracelet',
-    description: 'Cross jewelry is a trend that never really goes out of style! So, a cross bracelet is a must-have in your bracelet collection. This gold-colored bracelet features a beautiful minimalist cross charm. You can wear the cross because it holds a special meaning for you—or simply because you think it\'s a beautiful symbol!Have you completely fallen in love with this gold cross bracelet? Then be sure to check out our necklaces too—there's a matching necklace available!',
+    description: 'Cross jewelry is a trend that never really goes out of style! So, a cross bracelet is a must-have in your bracelet collection. This gold-colored bracelet features a beautiful minimalist cross charm. You can wear the cross because it holds a special meaning for you—or simply because you think it\'s a beautiful symbol!Have you completely fallen in love with this gold cross bracelet? Then be sure to check out our necklaces too—there\'s a matching necklace available!',
     mode: 'payment',
   },
 } as const;
