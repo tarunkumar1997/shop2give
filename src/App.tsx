aae5b7533ac4837f04b24898855f413cd96a004e
Elky Bachtiar <139325226+elky-bachtiar@users.noreply.github.com>
2025-06-01 10:20:24 +0200

    Remove non-existent ProductsPage references

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { AuthPage } from './pages/AuthPage';
 import { CampaignPage } from './pages/CampaignPage';
-import { CampaignsPage } from './pages/CampaignsPage';
-import { ProductsPage } from './pages/ProductsPage';
 import { CartPage } from './pages/CartPage';
 import { CreateCampaignPage } from './pages/CreateCampaignPage';
 import { SuccessPage } from './pages/SuccessPage';
@@ -16,10 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/campaigns" element={<CampaignsPage />} />
         <Route path="/campaigns/:slug" element={<CampaignPage />} />
         <Route path="/campaigns/create" element={<CreateCampaignPage />} />
-        <Route path="/products" element={<ProductsPage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/success" element={<SuccessPage />} />
         <Route path="/cancel" element={<CancelPage />} />
