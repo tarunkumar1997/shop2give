24784f728a89e3256550d6952df2581a4270dd1e
Elky Bachtiar <139325226+elky-bachtiar@users.noreply.github.com>
2025-06-03 09:09:53 +0200

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
           pink: '#FFF3F1',
           teal: {
             DEFAULT: '#4CBEB6',
-            dark: '#338B86',
+            dark: '#2A6D69',
             light: '#87D7CC'
           },
           charcoal: '#1A2C34',
